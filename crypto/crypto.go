f9c154a713b2174a80784a4843cf2c1b6dad4ac7
Fabio Colacio <fabio.colacio.dev@gmail.com>
2019-04-20 18:09:36 -0700

    Add hashandsaltpassword

---
+++ crypto/crypto.go
@@ -0,0 +1,15 @@
+package crypto
+
+import (
+    "golang.org/x/crypto/scrypt"
+)
+
+var (
+    KeyHashLength int = 32
+)
+
+// HashAndSaltPassword takes a password and salt, and creates a hash
+// of the password concatenated with the salt using Scrypt.
+func HashAndSaltPassword(passwd, salt []byte) ([]byte, error) {
+    return scrypt.Key(passwd, salt, 32768, 8, 1, KeyHashLength)
+}}
